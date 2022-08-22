83c09356f5c867f65e34e0bd44488e51ec26428e
Jeroen van Warmerdam <jeronevw@hotmail.com>
2022-08-22 13:30:02 +0200

    Print the Jest coverage report in Github Actions

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,20 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov"],
+  reporters: [
+    "default",
+    [
+      // Needed for CI workflow Github Action Job Summary.
+      // See https://github.com/dorny/test-reporter#supported-formats
+      "jest-junit",
+      {
+        outputDirectory: "coverage",
+        ancestorSeparator: " › ",
+        uniqueOutputName: "false",
+        suiteNameTemplate: "{filepath}",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}",
+      },
+    ],
+  ],
 };
